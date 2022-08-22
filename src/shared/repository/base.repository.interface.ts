024e2437e18d2b33c3e4293634b9108dd8ba1de3
MauroKinderknecht <56045994+MauroKinderknecht@users.noreply.github.com>
2022-08-22 18:10:12 -0300

    Update base.repository.interface.ts

--- src/shared/repository/base.repository.interface.ts
+++ src/shared/repository/base.repository.interface.ts
@@ -1,7 +1,12 @@
 export abstract class IBaseRepository<T extends { id: any }> {
   abstract create(data: any): Promise<T>;
-  abstract findAll(): Promise<T[]>;
+  abstract update(id: T["id"], data: any): Promise<T>;
+  abstract updateMany(where: any, data: any): Promise<T[]>;
+. abstract findAll(): Promise<T[]>;
   abstract findById(id: T['id']): Promise<T>;
-  abstract findMany(query: any): Promise<T[]>;
-  abstract findOne(query: any): Promise<T>;
+  abstract findOne(where: any, query?: any): Promise<T>;
+  abstract findMany(where: any, query?: any): Promise<T[]>;
+  abstract deleteById(id: T["id"]): Promise<T>;
+  abstract deleteOne(where: any): Promise<T>;
+  abstract deleteMany(where: any): Promise<T[]>;
 }
