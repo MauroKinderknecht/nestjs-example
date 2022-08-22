6851fa721414d3ef3b2438200fedc6bb57582a05
MauroKinderknecht <56045994+MauroKinderknecht@users.noreply.github.com>
2022-08-22 18:07:45 -0300

    Update base.repository.ts

--- src/shared/repository/base.repository.ts
+++ src/shared/repository/base.repository.ts
@@ -6,24 +6,44 @@ export class BaseRepository<T extends { id: any }> implements IBaseRepository<T>
   constructor(private readonly db: DatabaseService, private readonly model: string) {
     if (!Models.includes(model)) throw new InvalidModelError(`Model ${model} already exists`);
   }
-
   async create(data: any): Promise<T> {
-    return this.db[this.model].create({ data });
+    return this.service.db[this.model].create({ data });
+  }
+
+  async update(id: T["id"], data: any): Promise<T> {
+    return this.service.db[this.model].update({ where: { id }, data });
+  }
+
+  async updateMany(where: any, data: any): Promise<T[]> {
+    return this.service.db[this.model].update({ where, data });
   }
 
   async findAll(): Promise<T[]> {
-    return this.db[this.model].findMany();
+    return this.service.db[this.model].findMany();
   }
 
   async findById(id: T['id']): Promise<T> {
-    return this.db[this.model].findUnique({ where: { id } });
+    return this.service.db[this.model].findUnique({ where: { id } });
   }
 
-  async findMany(query: any): Promise<T[]> {
-    return this.db[this.model].findMany({ ...query });
+  async findOne(where: any, query?: any): Promise<T> {
+    return this.service.db[this.model].findFirst({ where, ...query });
   }
 
-  findOne(query: any): Promise<T> {
-    return this.db[this.model].findFirst({ ...query });
+  async findMany(where: any, query?: any): Promise<T[]> {
+    return this.service.db[this.model].findMany({ where, ...query });
   }
+
+  async deleteById(id: T["id"]): Promise<T> {
+    return this.service.db[this.model].delete({ where: { id }});
+  }
+
+  async deleteOne(where: any): Promise<T> {
+    return this.service.db[this.model].delete({ where });
+  }
+
+  async deleteMany(where: any): Promise<T[]> {
+    return this.service.db[this.model].deleteMany({ where });
+  }
+  
 }
